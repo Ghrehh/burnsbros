5d289066990e31662b491f224281f8c857f1e325
Ghrehh <gjburns9@hotmail.com>
2017-01-31 21:18:17 +0000

    first few sections of the about page

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
 const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
 
 gulp.task('nunjucks', function () {
     gulp.src('source/*.html')
+        .pipe(plumber())
         .pipe(nunjucks.compile())
         .pipe(gulp.dest('build'))
 });
 
 gulp.task('sass', function () {
-  return gulp.src('source/styles/*.scss')
+  return gulp.src('source/styles/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('build/styles'));
 });
