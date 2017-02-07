53c83cffcc72875c5778936b0fd95efcdeefa119
Ghrehh <gjburns9@hotmail.com>
2017-02-07 18:24:10 +0000

    added some links

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,3 +1,30 @@
+//taken from http://stackoverflow.com/questions/8425701/ajax-mailchimp-signup-form-integration
+function register($form) {
+  var mc_message = $(".mailchimp-message");
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        success     : function(data) {
+            if (data.result != "success") {
+                console.log("something went wrong")
+                mc_message.addClass("error");
+                mc_message.text("Something went wrong, please try again.");
+            } else {
+                console.log("all good")
+                mc_message.addClass("success");
+                mc_message.text("Please check your email to confirm your subscription.");
+                $(".email-field").val("");
+            }
+        }
+    });
+}
+
+
 $(document).ready(function(){
 
   $('#submission-form').submit(function(e) {
@@ -13,10 +40,10 @@ $(document).ready(function(){
             crossDomain: true,
             statusCode: {
                 0: function() {
-                    console.log("0")
+
                 },
                 200: function() {
-                    console.log("weh")
+
                 }
             }
 
@@ -40,5 +67,14 @@ $(document).ready(function(){
     e.preventDefault();
 
   });
+
+  var $form = $('#email-submission');
+
+  if ( $form.length > 0 ) {
+      $('form input[type="submit"]').bind('click', function ( event ) {
+          if ( event ) event.preventDefault();
+          register($form);
+      });
+  }
 
 });
