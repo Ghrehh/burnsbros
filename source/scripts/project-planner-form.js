d6a66089c6bcd7cfecaed221788f8c469dec4525
Ghrehh <gjburns9@hotmail.com>
2017-03-19 23:13:11 +0000

    sdf

--- source/scripts/project-planner-form.js
+++ source/scripts/project-planner-form.js
@@ -111,100 +111,124 @@ $(document).ready(function(){
 
   //custom form on click methods
 
-  $(".project-type .option").on("click touch", function(){
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $(".project-type .option").hover(function(){
 
-    if ($(this).hasClass("active-option")){
-      $(this).removeClass("active-option");
-    }
-    else {
-      $(this).addClass("active-option")
-    }
-    project_types = [];
+        if ($(this).hasClass("active-option")){
+          $(this).removeClass("active-option");
+        }
+        else {
+          $(this).addClass("active-option")
+        }
+        project_types = [];
 
-    $(".project-type .option").each(function(i, ele){
+        $(".project-type .option").each(function(i, ele){
+          if ($(this).hasClass("active-option")){
+            project_types.push($(this).data("value"))
+          }
+        })
+
+      })
+    }
+  else {
+
+    $(".project-type .option").click(function(){
+
       if ($(this).hasClass("active-option")){
-        project_types.push($(this).data("value"))
+        $(this).removeClass("active-option");
       }
+      else {
+        $(this).addClass("active-option")
+      }
+      project_types = [];
+
+      $(".project-type .option").each(function(i, ele){
+        if ($(this).hasClass("active-option")){
+          project_types.push($(this).data("value"))
+        }
+      })
+
     })
 
-  })
+    $(".start-date .option").click(function(){
+      $(".start-date .option").removeClass("active-option");
 
-  $(".start-date .option").click(function(){
-    $(".start-date .option").removeClass("active-option");
+      //hide all custom value fields and show default custom message
+      $(".start-date .option").find(".body-writing").css({"opacity": "1"});
+      $(".start-date .option").find(".calendar-field").css({"opacity": "0"});
 
-    //hide all custom value fields and show default custom message
-    $(".start-date .option").find(".body-writing").css({"opacity": "1"});
-    $(".start-date .option").find(".calendar-field").css({"opacity": "0"});
+      //if it's the custom field, hide the custom value message and display the custom value field
+      if ($(this).data("value") === "Custom") {
+        $(this).find(".body-writing").css({"opacity": "0"});
+        $(this).find(".calendar-field").css({"opacity": "1"});
+      }
 
-    //if it's the custom field, hide the custom value message and display the custom value field
-    if ($(this).data("value") === "Custom") {
-      $(this).find(".body-writing").css({"opacity": "0"});
-      $(this).find(".calendar-field").css({"opacity": "1"});
-    }
-
-    start_date = $(this).data("value");
+      start_date = $(this).data("value");
 
 
-    $(this).addClass("active-option")
-    
-  })
+      $(this).addClass("active-option")
+      
+    })
 
-  $(".deadline .option").click(function(){
-    $(".deadline .option").removeClass("active-option");
+    $(".deadline .option").click(function(){
+      $(".deadline .option").removeClass("active-option");
 
-    //hide all custom value fields and show default custom message
-    $(".deadline .option").find(".body-writing").css({"opacity": "1"});
-    $(".deadline .option").find(".calendar-field").css({"opacity": "0"});
+      //hide all custom value fields and show default custom message
+      $(".deadline .option").find(".body-writing").css({"opacity": "1"});
+      $(".deadline .option").find(".calendar-field").css({"opacity": "0"});
 
-    //if it's the custom field, hide the custom value message and display the custom value field
-    if ($(this).data("value") === "Custom") {
-      $(this).find(".body-writing").css({"opacity": "0"});
-      $(this).find(".calendar-field").css({"opacity": "1"});
+      //if it's the custom field, hide the custom value message and display the custom value field
+      if ($(this).data("value") === "Custom") {
+        $(this).find(".body-writing").css({"opacity": "0"});
+        $(this).find(".calendar-field").css({"opacity": "1"});
 
-    }
+      }
 
-    deadline = $(this).data("value");
+      deadline = $(this).data("value");
 
 
-    $(this).addClass("active-option")
-    
-    
-  })
+      $(this).addClass("active-option")
+      
+      
+    })
 
-  $(".budget-field .option").click(function(){
-    $(".budget-field .option").removeClass("active-option");
-    $(".budget-field .number").removeClass("active-number");
+    $(".budget-field .option").click(function(){
+      $(".budget-field .option").removeClass("active-option");
+      $(".budget-field .number").removeClass("active-number");
 
-    $(this).find(".number").addClass("active-number");
-    
+      $(this).find(".number").addClass("active-number");
+      
 
-    budget = String($(this).data("budget"));
-    var bar_filled_percentage = "0%";
+      budget = String($(this).data("budget"));
+      var bar_filled_percentage = "0%";
 
-    if ( budget === "<10000"){
-      bar_filled_percentage = "0%";
-      $(".budget-field .option-1").addClass("active-option");
-    }
-    else if ( budget === "20000"){
-      bar_filled_percentage = "25%";
-      $(".budget-field .option-1, .budget-field .option-2").addClass("active-option");
-    }
-    else if ( budget === "30000"){
-      bar_filled_percentage = "50%";
-      $(".budget-field .option-1, .budget-field .option-2, .budget-field .option-3").addClass("active-option");
-    }
-    else if ( budget === "40000"){
-      bar_filled_percentage = "75%";
-      $(".budget-field .option-1, .budget-field .option-2, .budget-field .option-3, .budget-field .option-4").addClass("active-option");
-    }
-    else if ( budget === "50000+"){
-      bar_filled_percentage = "100%";
-      $(".budget-field .option-1, .budget-field .option-2, .budget-field .option-3, .budget-field .option-4, .budget-field .option-5").addClass("active-option");
-    }
+      if ( budget === "<10000"){
+        bar_filled_percentage = "0%";
+        $(".budget-field .option-1").addClass("active-option");
+      }
+      else if ( budget === "20000"){
+        bar_filled_percentage = "25%";
+        $(".budget-field .option-1, .budget-field .option-2").addClass("active-option");
+      }
+      else if ( budget === "30000"){
+        bar_filled_percentage = "50%";
+        $(".budget-field .option-1, .budget-field .option-2, .budget-field .option-3").addClass("active-option");
+      }
+      else if ( budget === "40000"){
+        bar_filled_percentage = "75%";
+        $(".budget-field .option-1, .budget-field .option-2, .budget-field .option-3, .budget-field .option-4").addClass("active-option");
+      }
+      else if ( budget === "50000+"){
+        bar_filled_percentage = "100%";
+        $(".budget-field .option-1, .budget-field .option-2, .budget-field .option-3, .budget-field .option-4, .budget-field .option-5").addClass("active-option");
+      }
 
-    $(".bar-inner").animate({width: bar_filled_percentage}, 500);
+      $(".bar-inner").animate({width: bar_filled_percentage}, 500);
 
-  })
+    })
+  }
+
+  
 
   //add date pickers, jquery-ui code of the date pickers is added at the bottom of the
   $( ".calendar-field" ).datepicker({ dateFormat: 'dd-mm-yy' });
